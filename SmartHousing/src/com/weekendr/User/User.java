60c0dc3445cfa84a433e950b7b2052e8bc4d1efd
kakamble13052 <kshitij13052@iiitd.ac.in>
2014-07-28 17:39:31 +0530

    Smart3

--- SmartHousing/src/com/weekendr/User/User.java
+++ SmartHousing/src/com/weekendr/User/User.java
@@ -1,23 +1,63 @@
-package com.weekendr.User;
+package com.weekendr.user;
 
-import com.weekendr.Society.Society;
+public class User {
+protected String  name;
+protected double House_no;
+protected String houseId;
+protected double Phone_no;
+protected String Email_Id;
+protected String Username;
+protected String Password;
+protected String registration_Id;
+protected String getName() {
+	return name;
+}
+protected void setName(String name) {
+	this.name = name;
+}
+protected double getHouse_no() {
+	return House_no;
+}
+protected void setHouse_no(double house_no) {
+	House_no = house_no;
+}
+protected String getHouseId() {
+	return houseId;
+}
+protected void setHouseId(String houseId) {
+	this.houseId = houseId;
+}
+protected double getPhone_no() {
+	return Phone_no;
+}
+protected void setPhone_no(double phone_no) {
+	Phone_no = phone_no;
+}
+protected String getEmail_Id() {
+	return Email_Id;
+}
+protected void setEmail_Id(String email_Id) {
+	Email_Id = email_Id;
+}
+protected String getUsername() {
+	return Username;
+}
+protected void setUsername(String username) {
+	Username = username;
+}
+protected String getPassword() {
+	return Password;
+}
+protected void setPassword(String password) {
+	Password = password;
+}
+protected String getRegistration_Id() {
+	return registration_Id;
+}
+protected void setRegistration_Id(String registration_Id) {
+	this.registration_Id = registration_Id;
+}
 
-public class User implements Society{
-	protected String userId;
-	protected String password;
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
+
 
 }
